08e025ab4e0045be60c528a65c43b1081d16ec09
Jay-Patel-cg <jay.patel.a.cg@gmail.com>
2025-11-27 14:30:43 +0530

    JS Topic

---
+++ samir sir/3-11-2025/app.js
@@ -0,0 +1,41 @@
+// var hell = document.querySelector('.grandChild')
+
+// const style1 = {
+//     color: 'blue',
+//     backgroundColor: 'yellow',
+//     fontSize: '2rem',
+// };
+
+// Object.assign(hell.style, style1);
+
+// // console.log("hii");
+
+// var hello = "What You Doing ?";
+// document.querySelector('.parent').append(hello);
+
+
+// var ParaCreate1 = document.createElement('p');
+
+// ParaCreate1.textContent = "Hii";
+
+// document.querySelector('.child').append(ParaCreate1);
+
+// var hell = document.createElement('p');
+// hell.textContent = "Oreva Monkey D. Luffy ";
+// document.querySelector('.grandChild').append(hell);
+
+var container = document.querySelectorAll('div');
+
+var i = document.createElement('p'); 
+
+container.textContent = "hiiiiii";
+
+document.querySelector('div').append(container);
+
+
+
+
+
+
+
+
